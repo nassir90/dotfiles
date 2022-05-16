899cc0219651369480b7580ea72b4bfad229c5c5
Naza <nozolo90@gmail.com>
2022-05-16 02:27:29 +0100

    Haven't commit in a while

--- .zshrc
+++ .zshrc
@@ -9,11 +9,12 @@ autoload -Uz compinit
 compinit
 # End of lines added by compinstall
 # Lines configured by zsh-newuser-install
+fpath=(~/bin/zsh/{,completions} $fpath)
 HISTFILE=~/.histfile
 HISTSIZE=10000
 SAVEHIST=10000
 setopt autocd
-bindkey '' hgistory-incremental-pattern-search-backward
+bindkey '' history-incremental-pattern-search-backward
 bindkey -e
 prompt='%m %1d> '
 
@@ -26,28 +27,101 @@ alias la='ls -A'
 alias l='ls -I "*~"'
 alias l1='ls -1 -I "*~"'
 alias grep='grep --color=auto'
-alias vi=nvim
 alias loffice=libreoffice
 alias py=python3
-alias fl="ls $1 | fzf --multi"
-alias fcd='cd "`ls -1 | fzf`"'
 alias at=alacritty-themes
-alias em='emacs -nw'
-alias vi=em
+alias em='emacsclient'
+alias sb='sbcl --noinform --script'
+alias sbclr='rlwrap sbcl'
 
-# Functions
-
-function zf {
-    selection="`fl`"
-    if [ -n "$selection" ]
-    then
-        file "$selection" | grep -q PDF \
-            && zathura "$selection" \
-                || echo "'$selection' is not a PDF"
-    fi
+xdg-set-opener () {
+        [ ! ${#*} -eq 2 ] && return
+        application="$1"
+        mimetype=$(xdg-mime query filetype "$2")
+        echo -n "'$application'" will open "'$mimetype'". 'Continue? [y/N] '
+	read -k answer
+	echo
+	[ $answer = y ] && xdg-mime default "$application" "$mimetype" || echo 'Ok. Not changing anything.'
 }
 
-# Variables
+fcd () {
+    cd $(ff -d $*)
+}
+
+ff () {
+        local depth_arg=(-maxdepth 1)
+        local header_suffix="(cwd: $(pwd))"
+        local header="Pick a file"
+	local filename_format='%f'
+        
+        while getopts frdDs:m: name
+        do
+                case $name in
+                        f) type_arg=(-type f) ;;
+                        d) header="Pick a directory"
+                           implicit_fields=".."
+                           type_arg=(-type d) ;;
+                        s) header_suffix="$OPTARG" ;;
+                        m) depth_arg=(-maxdepth $OPTARG) ;;
+			D) filename_format='%h/%f' ;;
+                        r) filename_format='%h/%f'
+			   depth_arg= ;;
+			?) return 1 ;;
+                esac
+        done
+        
+        shift $[ OPTIND - 1 ]
+
+	local basename
+
+        find $1 $depth_arg $type_arg -printf "$filename_format\\n" \
+                | awk "{print} BEGIN {print \"$implicit_fields\"}" \
+                | fzf --header="$header $header_suffix" --header-first --multi \
+                | read basename
+
+	[ -n "$1" ] && echo $1/$basename || echo $basename
+}
+
+cd () {
+    [ -n "$1" ] && selection="$1" || selection=~
+    pushd -q "$selection" || return 1
+}
+
+dirstack () {
+    for dir in $dirstack ; do echo $dir ; done
+}
+
+scd () {
+    [ -z "$1" ] && dirstack | sort -u | grep -v "^$(pwd)$" | fzf --header-first --header="Pick a directory (cwd: $(pwd))" | read selection \
+	    || selection="$1"
+    cd "$selection"
+}
+
+fof () {
+    local suffix selection
+    
+    getopts n name && \
+	if [ $name = n ]
+	then
+	    suffix=\&
+	    1=
+	fi
+    
+    shift $[ $OPTIND - 1 ]
+    
+    [ -f "$1" ] && selection="$1" || ff -f "$1" | read selection
+
+    if [ -n "$selection" ] ; then
+        command="xdg-open '$selection'"
+        [ zsh = $(basename $SHELL) ] && print -s $command
+        $SHELL -c "$command $suffix"
+    fi      
+}
+
+help () {
+    [ -n "$1" ] && (cat /usr/share/zsh/5.8.1/help/$1 2>/dev/null || echo No help for "'$1'" found... && return 1)
+}
+
 export XDG_CONFIG_HOME="$HOME/.config"
 export XDG_DATA_HOME="$HOME/.local/share"
 export ANDROID_SDK_HOME="$HOME/packages/android-sdk/tools/"
--- init.el
+++ init.el
@@ -1,4 +1,3 @@
-(setq gnutls-algorithm-priority "NORMAL:-VERS-TLS1.3") ;; Possibly redundant
 (require 'package)
 (add-to-list 'package-archives '("melpa" . "https://melpa.org/packages/") t)
 (package-initialize)
@@ -15,7 +14,7 @@
    '("/home/naza/projects/python/wandb/subproject" "/home/naza/projects/python/wandb" "/media/projects/SpaceObjectVisualiser"))
  '(inhibit-startup-screen t)
  '(package-selected-packages
-   '(ein package-lint-flymake flycheck rust-mode go-mode hippie-exp-ext hippie-namespace hippie-expand-slime multiple-cursors dracula-theme beacon evil ess helm-company helm json-mode)))
+   '(cmake-mode elpy graphviz-dot-mode arduino-mode zzz-to-char lsp-java ejson-mode lsp-ui pianobar lsp-mode slime-company slime opencl-mode markdown-mode ein package-lint-flymake flycheck rust-mode go-mode hippie-exp-ext hippie-namespace hippie-expand-slime multiple-cursors dracula-theme beacon evil ess helm-company helm json-mode)))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
@@ -23,48 +22,66 @@
  ;; If there is more than one, they won't work right.
  )
 
+(global-set-key (kbd "C-c p") 'picture-mode)
+
+(setq lsp-rust-server 'rust-analyzer)
+
 (setq error-next-message-highlight t)
 (setq use-short-answers t)
-(setq scroll-margin 5)
-(setq scroll-conservatively 1)
+(setq scroll-margin 2
+      scroll-conservatively 1)
 
 (tool-bar-mode -1)
 (menu-bar-mode -1)
 
-(if (display-graphic-p) (load-theme 'dracula t))
+(load-theme 'modus-vivendi t)
 
 (setq backup-directory-alist '(("." . "~/.cache/emacs/saves/")))
 
+(load (expand-file-name "/usr/lib/quicklisp/slime-helper.el"))
+(setq inferior-lisp-program "sbcl")
+
+(add-hook 'rust-mode-hook 'lsp)
+(add-hook 'c++-mode-hook 'lsp)
+(add-hook 'c-mode-hook 'lsp)
 (add-hook 'go-mode-hook (lambda () (setq tab-width 2)))
-(add-hook 'org-mode-hook (lambda () (local-set-key (kbd "C-c o") 'org-latex-export-to-pdf)))
+(add-hook 'python-mode-hook #'elpy-enable)
+
+(defun org-mode-custom-settings ()
+  (local-set-key (kbd "C-c o p") #'org-latex-export-to-pdf)
+  (local-set-key (kbd "C-c o h") #'org-html-export-to-html))
+
+(add-hook 'org-mode-hook #'org-mode-custom-settings)
+
+(defun custom-java-mode-settings ()
+  (setq dabbrev-case-replace nil)) ; <3
+
+(add-hook 'java-mode-hook 'custom-java-mode-settings)
 
 (require 'helm-config)
 (helm-mode 1)
 
-(global-set-key (kbd "C-c m c") 'mc/edit-lines)
+(defvar helm-replacement-keys
+  '((helm-find-files . "C-x C-f")
+    (helm-buffers-list . "C-x b")
+    (helm-M-x . "M-x")))
+
+(global-set-key (kbd "C-c m") 'mc/edit-lines)
 (global-set-key (kbd "C-c <return>") 'find-file-at-point) ; Forgot about this
-(global-set-key (kbd "M-x") 'helm-M-x)
-(global-set-key (kbd "C-x C-f") 'helm-find-files)
 
 (defun pde-mode ()
   "Basically java mode with some custom features"
   (java-mode)
   (c-set-offset 'arglist-intro '+)
-  (setq c-basic-offset 2)
-  (setq tab-width 2))
+  (setq c-basic-offset 2
+	tab-width 2))
 
-(add-to-list
- 'auto-mode-alist
- '("\\.pde\\'" . pde-mode))
+(setq c-basic-offset 4)
 
-(add-hook
- 'asm-mode-hook
- (lambda ()
-   (setq tab-width 2)
-   (local-set-key (kbd "C-x a b") 'naza/add-csu-breakpoint)
-   (local-set-key (kbd "C-x a d") 'naza/remove-csu-breakpoint)))
+(add-to-list 'auto-mode-alist '("\\.pde\\'" . pde-mode))
+(add-to-list 'auto-mode-alist '("\\CMakeLists.txt\\'" . cmake-mode))
 
-(setq csu-config-file-name ".gdb")
+(defvar csu-config-file-name ".gdb")
 
 (defun naza/current-file-and-line-number (&optional delimiter)
   "Concatenates the current buffer name and the current line number"
@@ -94,48 +111,68 @@ Should fail if there is no file named .gdb in the current directory, so make it 
       (split-string (sh "svn status | sed -e '/^[^M]/d' -e 's/^M\s*//'"))
     (message (concat "You are not in an SVN directory: " (sh "echo -n `pwd`")))))
 
-(defun window-half-height ()
-  (max 1 (/ (1- (window-height (selected-window))) 2)))
-(defun scroll-up-half ()
-  (interactive)
+(defun window-half-height () (interactive) (max 1 (/ (1- (window-height (selected-window))) 2)))
+(defun scroll-up-half () (interactive) (scroll-up (window-half-height)))
+(defun scroll-down-half () (interactive) (scroll-down (window-half-height)))
 
-  (scroll-up (window-half-height)))
-(defun scroll-down-half ()
-  (interactive)
-  (scroll-down (window-half-height)))
-
-(global-set-key (kbd "M-n") 'next-error)
-(global-set-key (kbd "M-p") 'previous-error)
 (global-set-key (kbd "C-v") 'scroll-up-half)
 (global-set-key (kbd "M-v") 'scroll-down-half)
-
 (global-set-key (kbd "C-c v") 'evil-mode)
 (global-set-key (kbd "M-z") 'zap-up-to-char)
 
 (setq compilation-read-command nil)
 (setq compile-command "make")
 
-(global-unset-key (kbd "<C-left>"))
-(global-unset-key (kbd "<C-right>"))
-(global-unset-key (kbd "<C-up>"))
-(global-unset-key (kbd "<C-down>"))
-(global-unset-key (kbd "<M-left>"))
-(global-unset-key (kbd "<M-right>"))
-(global-unset-key (kbd "<M-up>"))
-(global-unset-key (kbd "<M-down>"))
+(dolist (key '("<C-left>" "<C-right>" "<C-up>" "<C-down>"
+	       "<M-left>" "<M-right>" "<M-up>" "<M-down>"))
+  (global-unset-key (kbd key)))
 
-(global-set-key (kbd "<up>") 'windmove-up)
-(global-set-key (kbd "C-x <up>") 'windmove-up)
-(global-set-key (kbd "<left>") 'windmove-left)
-(global-set-key (kbd "C-x <left>") 'windmove-left)
-(global-set-key (kbd "<down>") 'windmove-down)
-(global-set-key (kbd "C-x <down>") 'windmove-down)
-(global-set-key (kbd "<right>") 'windmove-right)
-(global-set-key (kbd "C-x <right>") 'windmove-right)
+(defvar window-movement-key-alist
+  '((windmove-right . "<right>")
+    (windmove-up . "<up>")
+    (windmove-left . "<left>")
+    (windmove-down . "<down>")))
+
+(defun prefixed-key-alist (prefix key-alist)
+  (mapcar #'(lambda (elem) (cons (car elem) (concat prefix " " (cdr elem)))) key-alist))
+
+(defun global-set-keys (key-alist)
+  (dolist (elem key-alist)
+    (global-set-key (kbd (cdr elem)) (car elem))))
+
+(defun local-set-keys (key-alist)
+  (dolist (elem key-alist)
+    (local-set-key (kbd (cdr elem)) (car elem))))
+
+(defvar logic-keybindings '(((lambda () (interactive) (insert-char 8707 1 t)) . "C-c e")
+			    ((lambda () (interactive) (insert-char 8704 1 t)) . "C-c f")
+			    ((lambda () (interactive) (insert-char 8743 1 t)) . "C-c a")
+			    ((lambda () (interactive) (insert-char 8744 1 t)) . "C-c o")
+			    ((lambda () (interactive) (insert-char 923 1 t)) . "C-c l")
+			    ((lambda () (interactive) (insert-char 8594 1 t)) . "C-c r")
+			    ((lambda () (interactive) (insert-char 8596 1 t)) . "C-c b")
+			    ((lambda () (interactive) (insert-char 955 1 t)) . "C-c u")))
+(global-set-keys logic-keybindings)
+
+(defun custom-asm-mode-settings ()
+  (setq tab-width 2)
+  (local-set-key (kbd "C-x a b") 'naza/add-csu-breakpoint)
+  (local-set-key (kbd "C-x a d") 'naza/remove-csu-breakpoint))
+
+(add-hook 'asm-mode-hook #'custom-asm-mode-settings)
+
+(defun custom-terminal-settings ()
+  (setq scroll-margin 0
+	scroll-conservatively 0))
+
+(add-hook 'term-mode-hook #'custom-terminal-settings)
+
+(global-set-keys (append window-movement-key-alist
+		   (prefixed-key-alist "C-x" window-movement-key-alist)
+		   helm-replacement-keys))
 
 (put 'upcase-region 'disabled nil)
 (put 'downcase-region 'disabled nil)
-
 (put 'scroll-left 'disabled nil)
 (put 'narrow-to-page 'disabled nil)
 (put 'narrow-to-region 'disabled nil)
